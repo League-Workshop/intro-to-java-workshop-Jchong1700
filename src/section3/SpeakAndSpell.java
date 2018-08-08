1cc715be37c47db0ff5549bfa4a373ba301de3bd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 14:53:04 -0700

    Day 3

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,15 +9,25 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("Spell osteoporosis");
 		// 2. Catch the user's answer in a String
-
+String answer = JOptionPane.showInputDialog("Spell!");
 		// 3. If the user spelled the word correctly, speak "correct"
+if (answer.equalsIgnoreCase("osteoporosis")) {speak("Correct!");
+} 
 
 		// 4. Otherwise say "wrong"
+else {speak("WRONG!");
 
+}
 		// 5. repeat the process for other words
+		speak("Spell silhouette");
+		String answer2 = JOptionPane.showInputDialog("Spell!");
+		if (answer2.equalsIgnoreCase("silhouette")) {speak("Correct!");
 		
+	}
+	else {speak("WRONG!");
+	}
 	}
 
 	static void speak(String words) {
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,41 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
+		int score = 0; 
 		
 		// 2.  Ask the user a question 
-		
+		String question = JOptionPane.showInputDialog("What is the square root of 25?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (question.equalsIgnoreCase("5")) {score = score + 1; }
+		else {JOptionPane.showMessageDialog(null, "Incorrect");}
 		// 4.  if the user's answer is correct
 		
 		// -- add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String question2 = JOptionPane.showInputDialog("Who is the Teenage Mutant Ninja Turtles' master ?");
 		
 		// 6.  After all the questions have been asked, print the user's score 
+		if (question2.equalsIgnoreCase("Splinter")) {score = score + 1; }
+		else {JOptionPane.showMessageDialog(null, "Incorrect");}
+		
+String question3 = JOptionPane.showInputDialog("What is 144/12 ?");
+		
+		if (question3.equalsIgnoreCase("12")) {score = score + 1; }
+		else {JOptionPane.showMessageDialog(null, "Incorrect");}
+		
+String question4 = JOptionPane.showInputDialog("Who is the best Teenage Mutant Ninja Turtle ?");
+		
+		if (question4.equalsIgnoreCase("Raphael")) {score = score + 1; }
+		else {JOptionPane.showMessageDialog(null, "Incorrect");}
+		
+		JOptionPane.showMessageDialog(null, "Your final score is " + score );
 		
 	}
 }
